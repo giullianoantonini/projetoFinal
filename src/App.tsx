de6d100f1523eaa7f619f6a15ba18d22ef451523
Thais Martinez Nunes <tmn@MacBook-Air-de-Thais.local>
2024-08-27 21:55:32 -0300

    feat: adiciona o primereact

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Button } from "primeReact/button";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 className="bg-orange-400">Projeto Final</h1>
+      <h1 className="lg:bg-[#0096D6] md:bg-orange-400 sm:bg-green-400 bg-slate-400 px-4 py-8 border-4 border-solid rounded border-black text-white">
+        Projeto Final
+      </h1>
+      <Button label="Like" icon="pi pi-heart-fill" severity="danger" />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { PrimeReactProvider } from "primereact/api";
+import "primeicons/primeicons.css";
 
-createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
+  </StrictMode>
+);
